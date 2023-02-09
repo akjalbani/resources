62c1b2058c1c955106ee0d9c5971ad1225867604
Akhtar Jalbani <akjalbani@yahoo.com>
2023-02-09 15:31:49 +1100

    Create login.php
    added

---
+++ Website/login.php
@@ -0,0 +1,12 @@
+<?php
+if($_SERVER['REQUEST_METHOD'] == 'POST') {
+  $username = $_POST['username'];
+  $password = $_POST['password'];
+
+  // Verify the password with the database
+
+  // Redirect to home page if password is correct
+
+  // Display error message if password is incorrect
+}
+?>
